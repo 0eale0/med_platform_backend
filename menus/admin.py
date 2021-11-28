5dc2ebb4ba632fd050a487a725fba72ca8f1950f
eale <eale@MacBook-Air-eale.local>
2021-11-28 17:40:17 +0300

    add models to admin panel

--- menus/admin.py
+++ menus/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Menu)
+admin.site.register(PatientMenu)
+admin.site.register(Day)
+admin.site.register(MenuDay)
+admin.site.register(Ingredient)
+admin.site.register(Dish)
+admin.site.register(DishIngredient)
+admin.site.register(DayDish)
