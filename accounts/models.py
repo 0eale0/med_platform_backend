d159d0bca2ea343a3a0dca8db7a190e73150422a
Ramil Suleymanov <lllumineux@outlook.com>
2021-11-28 19:34:26 +0300

    Add NULLABLE to models, delete null=False

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,9 @@ from django import forms
 from django.contrib.auth.models import AbstractUser
 
 
+NULLABLE = {'null': True, 'blank': True}
+
+
 class User(AbstractUser):
     middle_name = models.CharField(max_length=60)
 
@@ -10,21 +13,21 @@ class User(AbstractUser):
 class Patient(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
 
-    phone_number = models.CharField(max_length=100, null=False)
-    medical_card_number = models.CharField(max_length=255)
-    insurance_policy_number = models.CharField(max_length=100)
-    birth_date = models.DateField(null=False)
-    link_token = models.CharField(max_length=100, null=False)
+    phone_number = models.CharField(max_length=100)
+    medical_card_number = models.CharField(max_length=255, **NULLABLE)
+    insurance_policy_number = models.CharField(max_length=100, **NULLABLE)
+    birth_date = models.DateField()
+    link_token = models.CharField(max_length=100)
     sex = forms.ChoiceField(choices=['male', 'female'])
-    activity_level = models.CharField(max_length=255)
-    weight = models.FloatField()
-    waist = models.FloatField()
-    height = models.FloatField()
-    hips = models.IntegerField()
-    medical_information = models.TextField()
-    country = models.CharField(max_length=60, null=False)
-    city = models.CharField(max_length=60, null=False)
-    address = models.CharField(max_length=60, null=False)
+    activity_level = models.CharField(max_length=255, **NULLABLE)
+    weight = models.FloatField(**NULLABLE)
+    waist = models.FloatField(**NULLABLE)
+    height = models.FloatField(**NULLABLE)
+    hips = models.IntegerField(**NULLABLE)
+    medical_information = models.TextField(**NULLABLE)
+    country = models.CharField(max_length=60)
+    city = models.CharField(max_length=60)
+    address = models.CharField(max_length=60)
 
 
 class Doctor(models.Model):
@@ -37,5 +40,3 @@ class Doctor(models.Model):
 class DoctorPatient(models.Model):
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
-
-
--- menus/models.py
+++ menus/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 from accounts.models import Patient
 
 
@@ -12,8 +13,8 @@ class PatientMenu(models.Model):
 
 
 class Day(models.Model):
-    done = models.BooleanField(null=False)
-    number = models.IntegerField(null=False)
+    done = models.BooleanField(default=False, blank=True)
+    number = models.IntegerField()
 
 
 class MenuDay(models.Model):
@@ -22,34 +23,32 @@ class MenuDay(models.Model):
 
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=60, null=False)
+    name = models.CharField(max_length=60)
     default_weight = models.FloatField()
-    proteins = models.FloatField(null=False)
-    fats = models.FloatField(null=False)
-    carbohydrates = models.FloatField(null=False)
-    calories = models.FloatField(null=False)
+    proteins = models.FloatField()
+    fats = models.FloatField()
+    carbohydrates = models.FloatField()
+    calories = models.FloatField()
 
 
 class Dish(models.Model):
-    name = models.CharField(max_length=60, null=False)
+    name = models.CharField(max_length=60)
     default_weight = models.FloatField()
-    proteins = models.FloatField(null=False)
-    fats = models.FloatField(null=False)
-    carbohydrates = models.FloatField(null=False)
-    calories = models.FloatField(null=False)
+    proteins = models.FloatField()
+    fats = models.FloatField()
+    carbohydrates = models.FloatField()
+    calories = models.FloatField()
     recipe = models.TextField()
 
 
 class DishIngredient(models.Model):
     dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    amount = models.FloatField()
+    ingredient_amount = models.FloatField()
 
 
 class DayDish(models.Model):
     day = models.ForeignKey(Day, on_delete=models.CASCADE)
     dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
-    dish_amount = models.FloatField(null=False)
-    time = models.TimeField(null=False)
-
-
+    dish_amount = models.FloatField()
+    time = models.TimeField()
