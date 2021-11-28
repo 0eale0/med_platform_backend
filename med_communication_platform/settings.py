d79d72b5b68c6d6c33995db3d004273f821accf5
eale <eale@MacBook-Air-eale.local>
2021-11-28 17:49:25 +0300

    add migrations

--- med_communication_platform/settings.py
+++ med_communication_platform/settings.py
@@ -10,12 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import os
-import sys
 from pathlib import Path
-
-PROJECT_ROOT = os.path.dirname(__file__)
-sys.path.insert(0, os.path.join(PROJECT_ROOT, 'apps'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
