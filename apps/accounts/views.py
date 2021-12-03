ab9f90bfa5a5b4b03d11f79379c0f93c3b3ef922
Belligraf <belligraf@mail.ru>
2021-12-03 18:47:00 +0300

    + views

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,46 +1,12 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
-
-from apps.accounts.models import Patient
-from apps.accounts.serializers import PatientSerializer
+from rest_framework import viewsets, permissions
+from apps.accounts import serializers, models
 
 
-@csrf_exempt
-def patient_list(request):
-    if request.method == 'GET':
-        patient = Patient.objects.all()
-        serializer = PatientSerializer(patient, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = PatientSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+class UserView(viewsets.ModelViewSet):
+    queryset = models.User.objects.all()
+    serializer_class = serializers.UserSerializer
 
 
-@csrf_exempt
-def patient_detail(request, pk):
-    try:
-        patient = Patient.objects.get(pk=pk)
-    except Patient.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = PatientSerializer(patient)
-        return JsonResponse(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = PatientSerializer(patient, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        patient.delete()
-        return HttpResponse(status=204)
+class PatientView(viewsets.ModelViewSet):
+    queryset = models.Patient.objects.all()
+    serializer_class = serializers.PatientSerializer
