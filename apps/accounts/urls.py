5cc01426dcc2f1dc465e182d08f47ac6aa7bd7f6
Belligraf <belligraf@mail.ru>
2021-12-03 18:55:05 +0300

    + urls

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from sqlalchemy.testing.suite.test_reflection import users
+
 from apps.accounts import views
 
+router = routers.DefaultRouter()
+router.register('users', views.UserView)
+router.register('patient', views.PatientView)
+
+
 urlpatterns = [
-    path('patient/', views.patient_list),
-    path('patient/<int:pk>/', views.patient_detail),
+    path('api/account/', include(router.urls))
 ]
