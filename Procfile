834a2f47deadaa7cc604c8716eca1188e2407039
Ramil Suleymanov <lllumineux@outlook.com>
2021-12-06 17:14:29 +0300

    Add run.sh, whitenoise and gunicorn, configure Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: python manage.py runserver 0.0.0.0:8000
+web: bash ./run.sh
--- med_communication_platform/settings.py
+++ med_communication_platform/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'med_communication_platform.urls'
@@ -133,6 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
