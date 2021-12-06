e3bdddc255e0e5eb829ac9148171af9741c17348
Ramil Suleymanov <lllumineux@outlook.com>
2021-12-06 18:01:15 +0300

    Update run.sh, .gitlab-ci.yml

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
-# shellcheck disable=SC2046
-export $(xargs <.env)
-/app/.heroku/python/bin/python -m pip install --upgrade pip
+export $(cat .env)
 ./.heroku/python/bin/poetry config virtualenvs.create false
 ./.heroku/python/bin/poetry install --no-dev
 ./.heroku/python/bin/python manage.py migrate
 ./.heroku/python/bin/python manage.py collectstatic
-./.heroku/python/bin/gunicorn med_communication_platform.wsgi
+./.heroku/python/bin/gunicorn testdjango.wsgi
