a0511da27fa6712a5143061d732a263e2b12ff90
eale <novode25@gmail.com>
2021-12-19 22:06:48 +0300

    fix settigns py with black

--- med_communication_platform/settings.py
+++ med_communication_platform/settings.py
@@ -149,9 +149,7 @@ AUTH_USER_MODEL = "accounts.User"
 # Django rest
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ),
+    "DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework_simplejwt.authentication.JWTAuthentication",),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
 
