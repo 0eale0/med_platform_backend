44ebb6625361456e27cf57350d73a401df0ce080
eale <novode25@gmail.com>
2021-12-23 09:58:50 +0300

    add email authentificate instead username. Fix tools kit in menus/viewsets

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from apps.accounts.models import User, Patient, Doctor
+
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = (
+        'email',
+        'is_staff',
+        'is_active',
+    )
+    list_filter = (
+        'email',
+        'is_staff',
+        'is_active',
+    )
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {'classes': ('wide',), 'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
 
 admin.site.register(User)
 admin.site.register(Patient)
