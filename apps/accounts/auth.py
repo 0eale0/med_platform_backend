53d3978d38994fc4de0543d64f36e4daa9dd8416
Ramil Suleimanov <lllumineux@outlook.com>
2022-01-09 21:13:43 +0300

    Fix username in UserProfileManager

--- apps/accounts/auth.py
+++ apps/accounts/auth.py
@@ -7,8 +7,9 @@ class UserProfileManager(BaseUserManager):
             raise ValueError('User must have an email address')
 
         email = self.normalize_email(email)
-        user = self.model(username=email, email=email)
+        user = self.model(email=email)
 
+        user.username = email
         user.set_password(password)
         user.save(using=self._db)
 
