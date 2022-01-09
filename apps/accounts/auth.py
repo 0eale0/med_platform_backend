919ebcef4089f20a6bb28217b6dd80053deee739
Ramil Suleimanov <lllumineux@outlook.com>
2022-01-09 20:42:35 +0300

    Fix createsuperuser username field

--- apps/accounts/auth.py
+++ apps/accounts/auth.py
@@ -7,7 +7,7 @@ class UserProfileManager(BaseUserManager):
             raise ValueError('User must have an email address')
 
         email = self.normalize_email(email)
-        user = self.model(email=email)
+        user = self.model(username=email, email=email)
 
         user.set_password(password)
         user.save(using=self._db)
