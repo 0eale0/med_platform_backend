67d5c07eaac16d60164329739e355c67f9418fc3
eale <RUSLIKPVP@yandex.ru>
2022-03-14 00:03:39 +0300

    add migrations

--- apps/menus/models.py
+++ apps/menus/models.py
@@ -52,5 +52,5 @@ class DishIngredient(models.Model):
 
 
 class AdditionalDayDish(models.Model):
-    main_day_dish = models.ForeignKey(DayDish, on_delete=models.CASCADE)
-    additional_day_dish = models.ForeignKey(DayDish, on_delete=models.CASCADE)
+    main_day_dish = models.ForeignKey(DayDish, on_delete=models.CASCADE, related_name='main_day_dish')
+    additional_day_dish = models.ForeignKey(DayDish, on_delete=models.CASCADE, related_name='additional_day_dish')
