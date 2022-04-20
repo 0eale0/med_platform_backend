0b6ad987feb3cc0fe780424594600abe499abe47
AidarRakhmatullin <nedopolz@gmail.com>
2022-04-20 20:09:29 +0300

    fix celery

--- med_communication_platform/celery.py
+++ med_communication_platform/celery.py
@@ -1,12 +1,9 @@
 from __future__ import absolute_import
 
-import os
-
 from celery import Celery
 from django.conf import settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'picha.settings')
-app = Celery('picha')
+app = Celery()
 
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
