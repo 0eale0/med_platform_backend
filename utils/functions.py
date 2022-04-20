b303b4f61fb412fea09117b298f9ac34522b3652
Belligraf <belligraf@mail.ru>
2022-04-20 23:49:57 +0300

    rename constants

--- utils/functions.py
+++ utils/functions.py
@@ -1,7 +1,6 @@
 from datetime import datetime, date
 
-CONSTANTS_FOR_MALE = [66.5, 13.75, 5.003, 6.775]
-CONSTANTS_FOR_FEMALE = [655.1, 9.563, 1.85, 4.676]
+CONSTANTS_FOR_MALE_OR_FEMALE = {'male': [66.5, 13.75, 5.003, 6.775], 'female': [655.1, 9.563, 1.85, 4.676]}
 CONSTANTS_FOR_ACTIVITY_LEVEL = {
     'почти нет активности': 1.2,
     'умеренные нагрузки': 1.375,
@@ -13,7 +12,7 @@ CONSTANTS_FOR_ACTIVITY_LEVEL = {
 
 def calculate_cpfc(height: float, weight: float, activity_level: str, birth_date: date, sex: str) -> float:
     age = (datetime.now().date() - birth_date).days // 365
-    constants = CONSTANTS_FOR_MALE if 'male' == sex else CONSTANTS_FOR_FEMALE
+    constants = CONSTANTS_FOR_MALE_OR_FEMALE[sex]
     return CONSTANTS_FOR_ACTIVITY_LEVEL[activity_level] * (
         constants[0] + weight * constants[1] + height * constants[2] - age * constants[3]
     )
