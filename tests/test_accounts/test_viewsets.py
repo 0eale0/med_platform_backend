6b27f13192a669f7da3966da3daf903f5af9985c
Belligraf <belligraf@mail.ru>
2022-04-22 22:42:44 +0300

    trying to write the first test, probably doesn't work

--- tests/test_accounts/test_viewsets.py
+++ tests/test_accounts/test_viewsets.py
@@ -1,6 +1,18 @@
 import pytest
+from django.test import TestCase
+
+from apps.accounts.viewsets import PatientViewForDoctor
 
 
-@pytest.mark.unit
-def test_s():
-    pass
+class ExampleTest(TestCase):
+    def test_te(self):
+        pass
+
+
+class PatientViewForDoctorTest(TestCase):
+    def test_get_list_patient(self):
+        viewset = PatientViewForDoctor.as_view(
+            {
+                'get': 'list'
+            }
+        )
