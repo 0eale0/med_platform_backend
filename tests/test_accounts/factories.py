b795e597a04ef59576f143b8f94364a8d5f56980
Belligraf <belligraf@mail.ru>
2022-04-23 18:21:40 +0300

    bug with DJANGO_SETTINGS_MODULE

--- tests/test_accounts/factories.py
+++ tests/test_accounts/factories.py
@@ -2,8 +2,8 @@ from model_bakery import baker
 
 from apps.accounts.models import User, Doctor, Patient
 
-user_doctor, user_patient1, user_patient2 = baker.make(User, _quantity=3)
+user_doctor, user_patient1, user_patient2 = baker.prepare(User, _quantity=3)
 
-baker.make(Doctor, user=user_doctor)
-baker.make(Patient, user=user_patient1)
-baker.make(Patient, user=user_patient2)
+doctor = baker.prepare(Doctor, user=user_doctor)
+patient1 = baker.prepare(Patient, user=user_patient1)
+patient2 = baker.prepare(Patient, user=user_patient2)
--- tests/test_accounts/test_viewsets.py
+++ tests/test_accounts/test_viewsets.py
@@ -1,13 +1,26 @@
-from django.test import TestCase
+import json
 
+import pytest
+from django.urls import reverse
+from django_mock_queries.mocks import MockSet
 from apps.accounts.viewsets import PatientViewForDoctor
+from tests.test_accounts.factories import patient1
+
+# pytestmark = [pytest.mark.urls('config.urls'), pytest.mark.unit]
 
 
-class ExampleTest(TestCase):
-    def test_te(self):
+class TestPatientViewForDoctor:
+    def test_get_list_patient(self, mocker, rf):
         pass
-
-
-class PatientViewForDoctorTest(TestCase):
-    def test_get_list_patient(self):
-        viewset = PatientViewForDoctor.as_view({'get': 'list'})
+        # url = reverse('currency-list')
+        # request = rf.get(url)
+        # qs = MockSet(
+        #     patient1
+        # )
+        # view = PatientViewForDoctor.as_view(
+        #     {'get': 'list'}
+        # )
+        # mocker.patch.object(PatientViewForDoctor, 'get_queryset', return_value=qs)
+        # response = view(request).render()
+        # assert response.status_code == 200
+        # assert len(json.loads(response.content)) == 3
