0fa9c28428077ae9db803458a06f374f9910d3a5
Belligraf <belligraf@mail.ru>
2022-04-24 22:13:19 +0300

    add class InitUsers

--- tests/test_accounts/test_viewsets.py
+++ tests/test_accounts/test_viewsets.py
@@ -7,29 +7,38 @@ pytestmark = pytest.mark.django_db
 
 
 @pytest.mark.django_db
-class TestPatientViewForDoctor(APITestCase):
+class InitUsers(APITestCase):
     def setUp(self):
-        user_doctor = UserFactory(email='doc@mail.ru')
-        self.doctor = DoctorFactory(user=user_doctor)
+        self.user_doctor = UserFactory(email='doc@mail.ru')
+        self.doctor = DoctorFactory(user=self.user_doctor)
 
-        user_patient1, user_patient2 = UserFactory(), UserFactory()
-        self.patient1 = PatientFactory(user=user_patient1, doctor=self.doctor)
-        self.patient2 = PatientFactory(user=user_patient2, doctor=self.doctor)
+        self.user_patient1, self.user_patient2 = UserFactory(email='patient@mail.ru'), UserFactory()
+        self.patient1 = PatientFactory(user=self.user_patient1, doctor=self.doctor)
+        self.patient2 = PatientFactory(user=self.user_patient2, doctor=self.doctor)
 
-        self.client = APIClient()
-        response = self.client.post("/api/auth/token/", {'email': 'doc@mail.ru', 'password': '123'})
+        self.doctor_authorized = APIClient()
+        self.patient_authorized = APIClient()
+        response_for_doctor = self.doctor_authorized.post("/api/auth/token/",
+                                                          {'email': 'doc@mail.ru', 'password': '123'})
+        response_for_patient = self.patient_authorized.post("/api/auth/token/",
+                                                            {'email': 'patient@mail.ru', 'password': '123'})
 
-        self.access_token = response.json()['access']
-        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.access_token)
+        self.access_token = response_for_doctor.json()['access']
+        self.doctor_authorized.credentials(HTTP_AUTHORIZATION='Bearer ' + self.access_token)
 
+        self.access_token = response_for_patient.json()['access']
+        self.patient_authorized.credentials(HTTP_AUTHORIZATION='Bearer ' + self.access_token)
+
+
+class TestPatientViewForDoctor(InitUsers):
     def test_get_list_patient(self):
         url = '/api/accounts/patient/'
-        response = self.client.get(url)
+        response = self.doctor_authorized.get(url)
         assert response.status_code == 200
         assert len(response.json()) == 2
 
     def test_get_info_about_patient(self):
         url = '/api/accounts/patient/1/'
-        response = self.client.get(url, {"patient_id": self.patient1.id})
+        response = self.patient_authorized.get(url, {"patient_id": self.patient1.id})
         assert response.status_code == 200
         assert response.json()["id"] == self.patient1.id
