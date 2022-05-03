57eca55671483c859cbcbf7f69a739ec408832d7
AidarRakhmatullin <nedopolz@gmail.com>
2022-05-03 11:04:56 +0300

    fix run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ export $(cat .env)
 ./.heroku/python/bin/poetry install --no-dev
 ./.heroku/python/bin/python manage.py migrate
 ./.heroku/python/bin/python manage.py collectstatic
-./.heroku/python/bin/gunicorn med_communication_platform.wsgi
+./.heroku/python/bin/gunicorn med_communication_platform.wsgi --daemon
 ./.heroku/python/bin/celery -A med_communication_platform worker -l INFO
