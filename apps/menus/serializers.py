e4e79f5d5a6600274718661cd911d0ca6cad9787
AidarRakhmatullin <nedopolz@gmail.com>
2022-05-17 13:04:11 +0300

    fix serializer

--- apps/menus/serializers.py
+++ apps/menus/serializers.py
@@ -100,39 +100,15 @@ class AdditionalAndDaySerializer(serializers.ModelSerializer):
 
 
 class DishAndTimeSerializer(serializers.ModelSerializer):
-    id = serializers.SerializerMethodField()
-    name = serializers.SerializerMethodField()
-    amount = serializers.SerializerMethodField()
-    proteins = serializers.SerializerMethodField()
-    fats = serializers.SerializerMethodField()
-    carbohydrates = serializers.SerializerMethodField()
-    calories = serializers.SerializerMethodField()
-    recipe = serializers.SerializerMethodField()
+    id = serializers.CharField(source='dish.id')
+    name = serializers.CharField(source='dish.name')
+    amount = serializers.CharField(source='dish.default_weight')
+    proteins = serializers.CharField(source='dish.proteins')
+    fats = serializers.CharField(source='dish.fats')
+    carbohydrates = serializers.CharField(source='dish.carbohydrates')
+    calories = serializers.CharField(source='dish.calories')
+    recipe = serializers.CharField(source='dish.recipe')
     additional_to = serializers.SerializerMethodField()
-
-    def get_id(self, obj):
-        return obj.dish.id
-
-    def get_name(self, obj):
-        return obj.dish.name
-
-    def get_proteins(self, obj):
-        return obj.dish.proteins
-
-    def get_fats(self, obj):
-        return obj.dish.fats
-
-    def get_carbohydrates(self, obj):
-        return obj.dish.carbohydrates
-
-    def get_calories(self, obj):
-        return obj.dish.calories
-
-    def get_recipe(self, obj):
-        return obj.dish.recipe
-
-    def get_amount(self, obj):
-        return obj.dish.default_weight
 
     def get_additional_to(self, obj):
         dishes = [day.dish for day in DayDish.objects.filter(additional_to=obj.dish_id).all()]
